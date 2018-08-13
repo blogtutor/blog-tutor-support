2fcdfbec5bcd2ec9038c9705ca4e779c5bd0be1e
Andrew Wilder <andrew@blogtutor.com>
2018-08-13 08:02:09 -0700

    update version constant to 0.3.8

--- blog-tutor-support.php
+++ blog-tutor-support.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Blog Tutor Support
  * Description: Adds the Blog Tutor support widget to your WordPress dashboard for easy access to our knowledge base and contact form.
- * Version: 	0.3.7
+ * Version: 	0.3.8
  * Author:      Blog Tutor
  * Author URI:  https://blogtutor.com
  * GitHub URI: 	blogtutor/blog-tutor-support
@@ -36,7 +36,7 @@ class Blog_Tutor_Support {
    *
    * @var string
    */
-  const VERSION = '0.3.7';
+  const VERSION = '0.3.8';
 
   /**
    * Instance of this class.

7ab80b758829bd462b33f3833110c8d4397027dc
Sergio Scabuzzo <sergio@thegreenmanproject.com>
2018-08-21 12:42:01 -0700

    Add CSS to remove help tab from front end. More WP compiant code.

--- includes/class-support-widget.php
+++ includes/class-support-widget.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+	exit;
 }
 
 /**
@@ -11,57 +11,66 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @author   Fernando Acosta
  */
 class Blog_Tutor_Support_Widget {
-  /**
-   * Initialize the widget.
-   */
-  public function __construct() {
-    add_action( 'admin_footer', array( $this, 'widget' ), 50 );
-  }
+	/**
+	 * Initialize the widget.
+	 */
+	public function __construct() {
+		if ( current_user_can( 'editor' ) || current_user_can( 'administrator' ) ) {
+			add_action( 'wp_footer', array( $this, 'widget' ), 50 );
+			add_action( 'admin_footer', array( $this, 'widget' ), 50 );
+		}
+	}
 
-  public function widget() {
-  $options = get_option( 'blog_tutor_support_settings', array() );
+	public function widget() {
+		$options = get_option( 'blog_tutor_support_settings', array() );
 
-    // test mode check
-    // if ( isset( $options['test_mode'] ) && ! current_user_can( 'manage_options' ) ) {
-    if ( ! current_user_can( 'manage_options' ) ) {
-      return;
-    }
+			// Load Support Hero Widget.
+		echo '<script async data-cfasync="false" src="https://d29l98y0pmei9d.cloudfront.net/js/widget.min.js?k=Y2xpZW50SWQ9MTk0OSZob3N0TmFtZT1ibG9ndHV0b3Iuc3VwcG9ydGhlcm8uaW8="></script>';
 
-    if ( isset( $options['embed_code'] ) ) {
-      echo $options['embed_code'];
-    }
+		if ( is_user_logged_in() ) {
+			$ajw_current_user = wp_get_current_user(); ?>
 
-    // if ( is_user_logged_in() && isset( $options['identify_users'] ) ) {
-    if ( is_user_logged_in() ) {
-      $ajw_current_user = wp_get_current_user();
-    ?>
-      <script type="text/javascript">
-      jQuery( window ).load( function( $ ) {
-        if ( window.supportHeroWidget != undefined ) {
-          var properties = {
-				 articleParameters: {  // These can be embedded in articles with {variable}
-					 username: '<?php echo $ajw_current_user->user_login; ?>',
-					 firstname: '<?php echo $ajw_current_user->user_firstname; ?>',
-					 lastname: '<?php echo $ajw_current_user->user_lastname; ?>',
-					 displayname: '<?php echo $ajw_current_user->display_name; ?>',
-					 useremail: '<?php echo $ajw_current_user->user_email; ?>',
-					 site_url: '<?php echo site_url(); ?>',
-					 sitename: '<?php echo get_bloginfo( 'name' ); ?>',
-					 currentpage: window.location.href
-				 },
-				 custom: {  // These are used to prefill the contact form
-					 customerId: 1234,
-					 userEmail: '<?php echo $ajw_current_user->user_email; ?>',
-					 name: '<?php echo $ajw_current_user->user_firstname . ' ' . $ajw_current_user->user_lastname; ?>'
+			<script type="text/javascript">
+			jQuery( window ).load( function( $ ) {
+				if ( window.supportHeroWidget != undefined ) {
+					var properties = {
+						articleParameters: {  // These can be embedded in articles with {variable}
+							username: '<?php echo $ajw_current_user->user_login; ?>',
+							firstname: '<?php echo $ajw_current_user->user_firstname; ?>',
+							lastname: '<?php echo $ajw_current_user->user_lastname; ?>',
+							displayname: '<?php echo $ajw_current_user->display_name; ?>',
+							useremail: '<?php echo $ajw_current_user->user_email; ?>',
+							site_url: '<?php echo site_url(); ?>',
+							sitename: '<?php echo get_bloginfo( 'name' ); ?>',
+							currentpage: window.location.href
+							},
+							custom: {  // These are used to prefill the contact form
+								customerId: 1234,
+								userEmail: '<?php echo $ajw_current_user->user_email; ?>',
+								name: '<?php echo $ajw_current_user->user_firstname . ' ' . $ajw_current_user->user_lastname; ?>'
+							}
+						};
+						window.supportHeroWidget.track( properties );
+
+					<?php if ( ! is_admin() ) { // Hide help tab on front end; still loading widget code so help link in menu still works. ?>
+						window.supportHeroWidget.hideWidget();
+					<?php } ?>
 				}
-         };
-         window.supportHeroWidget.track( properties );
-        }
-      });
-      </script>
-    <?php
-    }
-  }
+			});
+			</script>
+
+			<?php if ( ! is_admin() ) { ?>
+				<style type="text/css">
+				#supporthero-button { display: none !important; }
+				</style>
+			<?php	} ?>
+			<?php if ( is_admin() ) { ?>
+				<style type="text/css">
+				#plugin-information #supporthero-button, .wp-admin.wp-core-ui.update-php #supporthero-button { display: none !important; }
+				</style>
+			<?php	}
+		}
+	}
 }
 
 new Blog_Tutor_Support_Widget();

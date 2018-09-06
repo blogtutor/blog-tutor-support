76c30bc0ada03798d752b0f7732e363fa5fabe0f
Sergio Scabuzzo <sergio@thegreenmanproject.com>
2018-09-05 17:48:24 -0700

    Add check for correct user in admin notices.

--- includes/admin/class-support-admin.php
+++ includes/admin/class-support-admin.php
@@ -1,7 +1,8 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-  exit;
+	exit;
 }
+
 
 /**
  * Blog Tutor Support Admin.
@@ -12,172 +13,169 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Blog_Tutor_Support_Admin {
 
-  /**
-   * Initialize the settings.
-   */
-  public function __construct() {
-    add_action( 'admin_menu', array( $this, 'settings_menu' ), 59 );
-    add_action( 'admin_notices', array( $this, 'blog_tutor_support_message' ), 59 );
-    add_action( 'admin_init', array( $this, 'plugin_settings' ) );
-  }
+	/**
+	 * Initialize the settings.
+	 */
+	public function __construct() {
+		add_action( 'admin_menu', array( $this, 'settings_menu' ), 59 );
+		add_action( 'admin_init', array( $this, 'plugin_settings' ) );
+	}
 
-  /**
-   * Add the settings page.
-   */
-  public function settings_menu() {
-    $current_user = wp_get_current_user();
-    if ( strpos( $current_user->user_email, '@blogtutor.com' ) !== false OR strpos( $current_user->user_email, '@nerdpress.net' ) !== false ) {
-      add_options_page(
-        'Blog Tutor Support',
-        'Blog Tutor Support',
-        'manage_options',
-        'blog-tutor-support',
-        array( $this, 'html_settings_page' )
-      );
-    }
-  }
+	/**
+	 * Add the settings page.
+	 */
+	public function settings_menu() {
+		$current_user = wp_get_current_user();
+		if ( strpos( $current_user->user_email, '@blogtutor.com' ) !== false || strpos( $current_user->user_email, '@nerdpress.net' ) !== false ) {
+			add_action( 'admin_notices', array( $this, 'blog_tutor_support_message' ), 59 );
+			add_options_page(
+				'Blog Tutor Support',
+				'Blog Tutor Support',
+				'manage_options',
+				'blog-tutor-support',
+				array( $this, 'html_settings_page' )
+			);
+		}
+	}
 
-  public function blog_tutor_support_message() {
-    $option = get_option('blog_tutor_support_settings');
-    if ( ! empty( $option['admin_notice'] ) ) {
-      ?>
-      <div class="notice notice-success">
-        <p>Blog Tutor Support Notice: <strong><?php _e( $option['admin_notice'], 'blog-tutor-support' ); ?></strong></p>
-      </div>
-      <?php
-    }
-  }
+	public function blog_tutor_support_message() {
+		$option = get_option('blog_tutor_support_settings');
+		if ( ! empty( $option['admin_notice'] ) ) {
+			?>
+			<div class="notice notice-success">
+				<p>Blog Tutor Support Notice: <strong><?php sanitize_text_field( $option['admin_notice'] ); ?></strong></p>
+			</div>
+			<?php
+		}
+	}
 
-  /**
-   * Render the settings page for this plugin.
-   */
-  public function html_settings_page() {
-    include dirname( __FILE__ ) . '/views/html-settings-page.php';
-  }
+	/**
+	 * Render the settings page for this plugin.
+	 */
+	public function html_settings_page() {
+		include dirname( __FILE__ ) . '/views/html-settings-page.php';
+	}
 
-  /**
-   * Plugin settings form fields.
-   */
-  public function plugin_settings() {
-    $option = 'blog_tutor_support_settings';
+	/**
+	 * Plugin settings form fields.
+	 */
+	public function plugin_settings() {
+		$option = 'blog_tutor_support_settings';
 
-    // Set Custom Fields cection.
-    add_settings_section(
-      'options_section',
-      __( 'Blog Tutor Support Section', 'blog-tutor-support' ),
-      array( $this, 'section_options_callback' ),
-      $option
-    );
+		// Set Custom Fields cection.
+		add_settings_section(
+			'options_section',
+			__( 'Blog Tutor Support Section', 'blog-tutor-support' ),
+			array( $this, 'section_options_callback' ),
+			$option
+		);
 
-    // add_settings_field(
-    //   'test_mode',
-    //   __( 'Test mode', 'blog-tutor-support' ),
-    //   array( $this, 'checkbox_element_callback' ),
-    //   $option,
-    //   'options_section',
-    //   array(
-    //     'menu'  => $option,
-    //     'id'    => 'test_mode',
-    //     'label' => __( 'If checked show the widget to admins only.', 'blog-tutor-support' ),
-    //   )
-    // );
+		//   'test_mode',
+		// add_settings_field(
+		//   __( 'Test mode', 'blog-tutor-support' ),
+		//   array( $this, 'checkbox_element_callback' ),
+		//   $option,
+		//   'options_section',
+		//   array(
+		//     'menu'  => $option,
+		//     'id'    => 'test_mode',
+		//     'label' => __( 'If checked show the widget to admins only.', 'blog-tutor-support' ),
+		//   )
+		// );
 
-    add_settings_field(
-      'admin_notice',
-      __( 'Blog Tutor Support Notice', 'blog-tutor-support' ),
-      array( $this, 'textarea_element_callback' ),
-      $option,
-      'options_section',
-      array(
-        'menu' => $option,
-        'id' => 'admin_notice',
-        'description' => __( 'Enter notice that will show for Blog Tutor only.', 'blog-tutor-support' ),
-      )
-    );
+		add_settings_field(
+			'admin_notice',
+			__( 'Blog Tutor Support Notice', 'blog-tutor-support' ),
+			array( $this, 'textarea_element_callback' ),
+			$option,
+			'options_section',
+			array(
+				'menu'        => $option,
+				'id'          => 'admin_notice',
+				'description' => __( 'Enter notice that will show for Blog Tutor only.', 'blog-tutor-support' ),
+			)
+		);
 
-    // add_settings_field(
-    //   'identify_users',
-    //   __( 'Identify Users', 'blog-tutor-support' ),
-    //   array( $this, 'checkbox_element_callback' ),
-    //   $option,
-    //   'options_section',
-    //   array(
-    //     'menu'  => $option,
-    //     'id'    => 'identify_users',
-    //     'label' => __( 'If checked Blog Tutor Support widget will identify the user ID, email and display name from logged users.', 'blog-tutor-support' ),
-    //   )
-    // );
+		// add_settings_field(
+		//   'identify_users',
+		//   __( 'Identify Users', 'blog-tutor-support' ),
+		//   array( $this, 'checkbox_element_callback' ),
+		//   $option,
+		//   'options_section',
+		//   array(
+		//     'menu'  => $option,
+		//     'id'    => 'identify_users',
+		//     'label' => __( 'If checked Blog Tutor Support widget will identify the user ID, email and display name from logged users.', 'blog-tutor-support' ),
+		//   )
+		// );
 
-    // Register settings.
-    register_setting( $option, $option, array( $this, 'validate_options' ) );
-  }
+		// Register settings.
+		register_setting( $option, $option, array( $this, 'validate_options' ) );
+	}
 
-  /**
-   * Section null fallback.
-   */
-  public function section_options_callback() {
+	/**
+	 * Section null fallback.
+	 */
+	public function section_options_callback() {}
 
-  }
+	/**
+	 * Checkbox element fallback.
+	 *
+	 * @param array $args Callback arguments.
+	 */
+	public function checkbox_element_callback( $args ) {
+		$menu    = $args['menu'];
+		$id      = $args['id'];
+		$options = get_option( $menu );
 
-  /**
-   * Checkbox element fallback.
-   *
-   * @param array $args Callback arguments.
-   */
-  public function checkbox_element_callback( $args ) {
-    $menu    = $args['menu'];
-    $id      = $args['id'];
-    $options = get_option( $menu );
+		if ( isset( $options[ $id ] ) ) {
+			$current = $options[ $id ];
+		} else {
+			$current = isset( $args['default'] ) ? $args['default'] : '0';
+		}
 
-    if ( isset( $options[ $id ] ) ) {
-      $current = $options[ $id ];
-    } else {
-      $current = isset( $args['default'] ) ? $args['default'] : '0';
-    }
+		include dirname( __FILE__ ) . '/views/html-checkbox-field.php';
+	}
 
-    include dirname( __FILE__ ) . '/views/html-checkbox-field.php';
-  }
+	/**
+	 * Textarea element fallback.
+	 *
+	 * @param array $args Callback arguments.
+	 */
+	public function textarea_element_callback( $args ) {
+		$menu    = $args['menu'];
+		$id      = $args['id'];
+		$options = get_option( $menu );
 
-  /**
-   * Textarea element fallback.
-   *
-   * @param array $args Callback arguments.
-   */
-  public function textarea_element_callback( $args ) {
-    $menu    = $args['menu'];
-    $id      = $args['id'];
-    $options = get_option( $menu );
+		if ( isset( $options[ $id ] ) ) {
+			$value = $options[ $id ];
+		} else {
+			$value = isset( $args['default'] ) ? $args['default'] : '';
+		}
 
-    if ( isset( $options[ $id ] ) ) {
-      $value = $options[ $id ];
-    } else {
-      $value = isset( $args['default'] ) ? $args['default'] : '';
-    }
+		include dirname( __FILE__ ) . '/views/html-textarea-field.php';
+	}
 
-    include dirname( __FILE__ ) . '/views/html-textarea-field.php';
-  }
+	/**
+	 * Valid options.
+	 *
+	 * @param  array $input options to valid.
+	 *
+	 * @return array        validated options.
+	 */
+	public function validate_options( $input ) {
+		$output = array();
 
-  /**
-   * Valid options.
-   *
-   * @param  array $input options to valid.
-   *
-   * @return array        validated options.
-   */
-  public function validate_options( $input ) {
-    $output = array();
+		// Loop through each of the incoming options.
+		foreach ( $input as $key => $value ) {
+			// Check to see if the current option has a value. If so, process it.
+			if ( isset( $input[ $key ] ) && ! empty( $input[ $key ] ) ) {
+				$output[ $key ] = $input[ $key ];
+			}
+		}
 
-    // Loop through each of the incoming options.
-    foreach ( $input as $key => $value ) {
-      // Check to see if the current option has a value. If so, process it.
-      if ( isset( $input[ $key ] ) && ! empty( $input[ $key ] ) ) {
-        $output[ $key ] = $input[ $key ];
-      }
-    }
-
-    return $output;
-  }
-
+		return $output;
+	}
 }
 
-new Blog_Tutor_Support_Admin;
+new Blog_Tutor_Support_Admin();

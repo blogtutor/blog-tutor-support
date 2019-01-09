db6552fb8b32b902695abe0cdf12af04b59a11a6
Sergio Scabuzzo <sergio@thegreenmanproject.com>
2019-01-08 16:09:13 -0800

    Clear page from Cloudproxy menu item. Close #22

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -1,121 +1,125 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+  exit;
 }
 
-// Add Admin Bar Menu Items.
+// Add Admin Bar Menu Items
+
 function bt_custom_toolbar_links( $wp_admin_bar ) {
 
-	if ( current_user_can( 'editor' ) || current_user_can( 'administrator' ) ) {
-		// Add "NerdPress" parent menu Items.
-		$args = array(
-			'id'     => 'nerdpress-menu',
-			'title'  => 'NerdPress',
-			'parent' => false,
-		);
-		$wp_admin_bar->add_node( $args );
+  if ( current_user_can('editor') || current_user_can('administrator') ) {
 
-		// Add Child Menu Items.
-		// Add a Clear Cloudproxy link to the Admin Bar.
-		if ( file_exists( $_SERVER['DOCUMENT_ROOT'] . '/wp-content/uploads/sucuri/sucuri-settings.php' ) ) {
+      // Add "Blog Tutor" parent menu Items
+      $args = array(
+      'id'      => 'blog-tutor-menu',
+      'title'   =>  'Blog Tutor',
+      'parent'  => false,
+      );
+      $wp_admin_bar->add_node( $args );
 
-			// Get Cloudproxy API Keys.
-			$input_lines = file_get_contents( $_SERVER['DOCUMENT_ROOT'] . '/wp-content/uploads/sucuri/sucuri-settings.php' );
-			// Using # as regex delimiters since / was giving error.
-			$regex = "#\"sucuriscan_cloudproxy_apikey\":\"(.{32})\\\/(.{32})#";
+      // Add Child Menu Items
+      // Add a Clear Cloudproxy link to the Admin Bar
+      if ( file_exists($_SERVER[ "DOCUMENT_ROOT" ] . '/wp-content/uploads/sucuri/sucuri-settings.php' ) ) {
 
-			preg_match_all( $regex, $input_lines, $output_array, PREG_SET_ORDER, 0 );
+        // Get Cloudproxy API Keys
+        $input_lines = file_get_contents( $_SERVER[ "DOCUMENT_ROOT" ] . '/wp-content/uploads/sucuri/sucuri-settings.php' );
+        // Using # as regex delimiters since / was giving error
+        $regex = "#\"sucuriscan_cloudproxy_apikey\":\"(.{32})\\\/(.{32})#";
 
-			if ( array_filter( $output_array ) ) {
-				$api_key    = $output_array[0][1];
-				$api_secret = $output_array[0][2];
-			}
+        preg_match_all( $regex, $input_lines, $output_array, PREG_SET_ORDER, 0 );
 
-			if ( isset( $api_key ) ) {
-				// Build the Clear Cache & Whitelist links (Cloudproxy API v1) and add it to the admin bar.
-				$cloudproxy_clear = 'https://waf.sucuri.net/api?&k=' . $api_key . '&s=' . $api_secret . '&a=clearcache';
-				$args             = array(
-					'id'     => 'bt-clear-cloudproxy',
-					'title'  => 'Clear Cloudproxy Cache',
-					'href'   => $cloudproxy_clear,
-					'parent' => 'nerdpress-menu',
-					'meta'   => array(
-						'class'  => 'btButton',
-						'target' => 'blank',
-						'title'  => 'Clear the Cloudproxy cache',
-					),
-				);
+        if ( array_filter($output_array) ) {
+          $API_KEY = $output_array[0][1];
+          $API_SECRET = $output_array[0][2];
+        }
 
-				$wp_admin_bar->add_node( $args );
-				$cloudproxy_whitelist = 'https://waf.sucuri.net/api?&k=' . $api_key . '&s=' . $api_secret . '&a=whitelist';
-				$args                 = array(
-					'id'     => 'bt-whitelist-cloudproxy',
-					'title'  => 'Whitelist Your IP Address',
-					'href'   => $cloudproxy_whitelist,
-					'parent' => 'nerdpress-menu',
-					'meta'   => array(
-						'class'  => 'btButton',
-						'target' => 'blank',
-						'title'  => 'Whitelist your current IP address, in case Cloudproxy is blocking you.',
-						'parent' => 'nerdpress-menu',
-					),
-				);
-				$wp_admin_bar->add_node( $args );
-			} else {
-				$args = array(
-					'id'     => 'bt-cloudproxy-api-not-set',
-					'title'  => 'Cloudproxy API Key is not set',
-					'parent' => 'nerdpress-menu',
-					'meta'   => array(
-						'class' => 'btButton',
-						'title' => 'Your Cloudproxy API key is not set in the Sucuri Plugin. If your site should be configured to use Cloudproxy, please contact us!',
-					),
-				);
-				$wp_admin_bar->add_node( $args );
-			}
-		} else {
-			$args = array(
-				'id'     => 'bt-sucuri-missing',
-				'title'  => 'The Sucuri Plugin is missing!',
-				'parent' => 'nerdpress-menu',
-				'meta'   => array(
-					'class' => 'btButton',
-					'title' => 'Your Sucuri Plugin is not configured. Please contact us!',
-				),
-			);
-			$wp_admin_bar->add_node( $args );
-		}
+        if ( isset ( $API_KEY ) ) {
+          // Build the Clear Cache & Whitelist links (Cloudproxy API v1) and add it to the admin bar
+          $Cloudproxy_clear = "https://waf.sucuri.net/api?&k=" . $API_KEY . "&s=" . $API_SECRET . "&a=clearcache";
+          $args = array(
+              'id' => 'bt-clear-cloudproxy',
+              'title' => 'Clear Cloudproxy Cache',
+              'href' => $Cloudproxy_clear,
+              'parent'  => 'blog-tutor-menu',
+              'meta' => array(
+                  'class' => 'btButton',
+                  'target' => 'blank',
+                  'title' => 'Clear the Cloudproxy cache',
+                  )
+          );
+          $wp_admin_bar->add_node( $args );
 
-		// "Get Help" link to open the Support Hero widget
-		$args = array(
-			'id'     => 'bt-get-help',
-			'title'  => 'Get Help',
-			'href'   => '#',
-			'parent' => 'nerdpress-menu',
-			'meta'   => array(
-				'class'   => 'btButton',
-				'title'   => 'Click to open our knowledge base and contact form.',
-				'onclick' => 'window.supportHeroWidget.show();',
-			),
-		);
-		$wp_admin_bar->add_node( $args );
+          $Cloudproxy_whitelist = "https://waf.sucuri.net/api?&k=" . $API_KEY . "&s=" . $API_SECRET . "&a=whitelist";
+          $args = array(
+              'id' => 'bt-whitelist-cloudproxy',
+              'title' => 'Whitelist Your IP Address',
+              'href' => $Cloudproxy_whitelist,
+              'parent'  => 'blog-tutor-menu',
+              'meta' => array(
+                  'class' => 'btButton',
+                  'target' => 'blank',
+                  'title' => 'Whitelist your current IP address, in case Cloudproxy is blocking you.',
+                  'parent'  => 'blog-tutor-menu'
+                  )
+          );
+          $wp_admin_bar->add_node( $args );
 
-		$current_user = wp_get_current_user();
-		if ( strpos( $current_user->user_email, '@blogtutor.com' ) !== false || strpos( $current_user->user_email, '@nerdpress.net' ) !== false ) {
+          //Clear page from Cloudproxy cache
+          $path = $_SERVER['REQUEST_URI'];
+          $Cloudproxy_clear_page_from_cache = "https://waf.sucuri.net/api?&k=" . $API_KEY . "&s=" . $API_SECRET . "&a=clearcache" . "&file=" . $path;
+          $args = array(
+              'id' => 'bt-clear-cache-cloudproxy',
+              'title' => 'Clear page from Cloudproxy',
+              'href' => $Cloudproxy_clear_page_from_cache,
+              'parent'  => 'blog-tutor-menu',
+              'meta' => array(
+                  'class' => 'btButton',
+                  'target' => 'blank',
+                  'title' => 'Clear this page from Cloudproxy cache.',
+                  'parent'  => 'blog-tutor-menu'
+                  )
+          );
+          $wp_admin_bar->add_node( $args );
 
-			// "Plugin Settings" link to open the NerdPress Support settings page.
-			$args = array(
-				'id'     => 'bt-settings',
-				'title'  => 'Plugin Settings',
-				'href'   => '/wp-admin/options-general.php?page=nerdpress-support',
-				'parent' => 'nerdpress-menu',
-				'meta'   => array(
-					'class' => 'btButton',
-					'title' => 'Open NerdPRess Support plugin settings.',
-				),
-			);
-			$wp_admin_bar->add_node( $args );
-		}
-	}
+        } else {
+            $args = array(
+              'id' => 'bt-cloudproxy-api-not-set',
+              'title' => 'Cloudproxy API Key is not set',
+              'parent'  => 'blog-tutor-menu',
+              'meta' => array(
+                  'class' => 'btButton',
+                  'title' => 'Your Cloudproxy API key is not set in the Sucuri Plugin. If your site should be configured to use Cloudproxy, please contact us!'
+                  )
+          );
+          $wp_admin_bar->add_node( $args );
+        }
+      } else {
+          $args = array(
+            'id' => 'bt-sucuri-missing',
+            'title' => 'The Sucuri Plugin is missing!',
+            'parent'  => 'blog-tutor-menu',
+            'meta' => array(
+                'class' => 'btButton',
+                'title' => 'Your Sucuri Plugin is not configured. Please contact us!'
+                )
+        );
+       $wp_admin_bar->add_node( $args );
+      }
+
+      // "Get Help" link to open the Support Hero widget
+        $args = array(
+            'id' => 'bt-get-help',
+            'title' => 'Get Help',
+            'href' => '#',
+            'parent'  => 'blog-tutor-menu',
+            'meta' => array(
+                'class' => 'btButton',
+                'title' => 'Click to open our knowledge base and contact form.',
+                'onclick' => 'window.supportHeroWidget.show();'
+                )
+        );
+        $wp_admin_bar->add_node( $args );
+
+  }
 }
 add_action( 'admin_bar_menu', 'bt_custom_toolbar_links', 99 );

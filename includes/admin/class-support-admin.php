2317fe3c1daf9ab8b8530110387f1f3e959f21d9
Andrew Wilder <andrew@blogtutor.com>
2019-01-19 05:41:44 -0800

    Add parentheses to is_nerdpress() for clarity of operator precedence.

--- includes/admin/class-support-admin.php
+++ includes/admin/class-support-admin.php
@@ -23,7 +23,7 @@ class Blog_Tutor_Support_Admin {
 
 	public static function is_nerdpress() {
 		$current_user = wp_get_current_user();
-		if ( current_user_can( 'administrator' ) && strpos( $current_user->user_email, '@blogtutor.com' ) !== false || strpos( $current_user->user_email, '@nerdpress.net' ) !== false ) {
+		if ( current_user_can( 'administrator' ) && ( strpos( $current_user->user_email, '@blogtutor.com' ) !== false || strpos( $current_user->user_email, '@nerdpress.net' ) !== false ) ) {
 		 return true;
 	 }	else {
 		 return false;

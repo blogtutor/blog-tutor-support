991bc468a8d18c152e45e141c95eb3f1725a478e
Sergio Scabuzzo <sergio@ecotechie.io>
2021-07-21 13:42:28 -0700

    Fix JS Exclusion list in WP Rocket for AdThrive and Mediavine.

--- blog-tutor-support.php
+++ blog-tutor-support.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: NerdPress Support
  * Description: Helps your site work with our custom Cloudflare Enterprise setup or the Sucuri Firewall, and adds the NerdPress "Need Help?" support tab to your dashboard.
- * Version:     1.0.4-beta1
+ * Version:     1.0.4
  * Author:      NerdPress
  * Author URI:  https://www.nerdpress.net
  * GitHub URI: 	blogtutor/blog-tutor-support
@@ -22,7 +22,7 @@ include( dirname( __FILE__ ) . '/github-updater.php' );
 include( dirname( __FILE__ ) . '/includes/admin-menu.php' );
 
 if ( ! defined( 'BT_PLUGIN_VERSION' ) ) {
-	define( 'BT_PLUGIN_VERSION', '1.0.4-beta1' );
+	define( 'BT_PLUGIN_VERSION', '1.0.4' );
 }
 
 if ( ! class_exists( 'Blog_Tutor_Support' ) ) :
@@ -95,8 +95,8 @@ class Blog_Tutor_Support {
 					$excluded_strings[] = "ga\('";
 					$excluded_strings[] = "gtag\(";
 					$excluded_strings[] = 'gtagTracker'; // Monster Insights
-					$excluded_strings[] = "scripts\.mediavine\.com";
-					$excluded_strings[] = "ads\.adthrive\.com";
+					$excluded_strings[] = "mediavine";
+					$excluded_strings[] = "adthrive";
 					$excluded_strings[] = "nutrifox";
 					$excluded_strings[] = "flodesk";
 					$excluded_strings[] = "cp-popup\.js"; // ConvertPro

76f1387da750f5aeddd94dddb12ec390b3c619a0
Sergio Scabuzzo <sergio@ecotechie.io>
2021-08-01 13:52:18 -0700

    Remove Sucuri Allowlist and Clear Cache form fields. Closes #145

--- includes/admin/views/html-sucuri-options-field.php
+++ includes/admin/views/html-sucuri-options-field.php
@@ -2,33 +2,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-?>
 
-<h2>Add IP to the allowlist:</h2>
-<p>Adds defined IP address to the allowlist so it won't be blocked by some of Sucuri's Firewall security rules.</p>
-<form method="get" class="form-loader" target="_blank" action="<?php echo htmlspecialchars( $sucuri_api_call_array['address'] ); ?>">
-	<div class="input-group">
-		<input type="hidden" id="k" name="k" value="<?php echo $sucuri_api_call_array['api_key']; ?>">
-		<input type="hidden" id="s" name="s" value="<?php echo $sucuri_api_call_array['api_secret']; ?>">
-		<input type="hidden" id="a" name="a" value="allowlist">
-		<input type="text" id="ip" name="ip" placeholder="Add new IP...">
-		<input type="submit" value="Allowlist">
-	</div>
-</form>
-<h2>Clear Cache per File:</h2>
-<p>This option can be used to remove a file from the Sucuri Firewall cache.</p>
-<p>This will reflect live as soon as you click the clear cache button.</p>
-<form method="get" class="form-loader" target="_blank" action="<?php echo htmlspecialchars( $sucuri_api_call_array['address'] ); ?>">
-	<div class="input-group">
-		<input type="hidden" id="k" name="k" value="<?php echo $sucuri_api_call_array['api_key']; ?>">
-		<input type="hidden" id="s" name="s" value="<?php echo $sucuri_api_call_array['api_secret']; ?>">
-		<input type="hidden" id="a" name="a" value="clearcache">
-		<input type="text" id="file" name="file" placeholder="File Path...">
-		<input type="submit" value="Clear File">
-	</div>
-</form>
-
-<?php
 $allowlist_ips = get_option( 'cloudproxy_allowlist_ips', array() );
 
 if( count( $allowlist_ips ) > 0 ) {	

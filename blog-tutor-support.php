d653fd136b5d882ec1efc813d664d7422023e2ea
Sergio Scabuzzo <sergio@ecotechie.io>
2021-08-26 15:34:19 -0700

    Move override methods to their own class. Override alerts emails. Closes #142. Refactor some code...

--- blog-tutor-support.php
+++ blog-tutor-support.php
@@ -43,12 +43,6 @@ class NerdPress {
 	 * Initialize the plugin.
 	 */
 	private function __construct() {
-		// Load plugin text domain.
-		add_action( 'init', array( $this, 'check_options' ) );
-		if ( class_exists( 'WooCommerce' ) ) {
-			add_filter( 'woocommerce_background_image_regeneration', '__return_false' );
-		}
-	
 		// Include classes.
 		$this->includes();
 
@@ -72,57 +66,6 @@ class NerdPress {
 	}
 
 	/**
-	 * Check mandatory options, set to default if not present
-	 */
-	public function check_options() {
-		$option_array = 'blog_tutor_support_settings';
-		$bt_opts      = get_option( $option_array, array() );
-
-		// Perform the check only if logged in
-		if ( ! is_admin() ) {
-			if ( ! isset( $bt_opts['exclude_wp_rocket_delay_js'] ) ) {
-				// Exclude scripts from WP Rocket JS delay.
-				function np_wp_rocket__exclude_from_delay_js( $excluded_strings = array() ) {
-					// MUST ESCAPE PERIODS AND PARENTHESES!
-					$excluded_strings[] = 'google-analytics';
-					$excluded_strings[] = "/gtag/";
-					$excluded_strings[] = "/gtm\.js";
-					$excluded_strings[] = "/gtm-";
-					$excluded_strings[] = "ga\( '";
-					$excluded_strings[] = "ga\('";
-					$excluded_strings[] = "gtag\(";
-					$excluded_strings[] = 'gtagTracker'; // Monster Insights
-					$excluded_strings[] = "mediavine";
-					$excluded_strings[] = "adthrive";
-					$excluded_strings[] = "nutrifox";
-					$excluded_strings[] = "flodesk";
-					$excluded_strings[] = "cp-popup\.js"; // ConvertPro
-					$excluded_strings[] = "wp-recipe-maker";
-					$excluded_strings[] = "slickstream";
-					$excluded_strings[] = "social-pug";
-					return $excluded_strings;
-				}
-
-				add_filter( 'rocket_delay_js_exclusions', 'np_wp_rocket__exclude_from_delay_js' );
-  		}
-
-			return;
-		}
-
-		$default_opts = array(
-			'firewall_choice' => 'none',
-			'cloudflare_zone' => 'dns1',
-		); 
-
-		foreach ( $default_opts as $key => $val ) {
-			if ( !array_key_exists( $key, $bt_opts ) || !isset( $bt_opts[$key] ) )
-				$bt_opts[$key] = $val; 
-		}
-
-		update_option( $option_array, $bt_opts );
-	} 
-
-	/**
 	 * Include admin actions.
 	 */
 	protected function admin_includes() {
@@ -135,15 +78,17 @@ class NerdPress {
 	protected function includes() {
 		include_once dirname( __FILE__ ) . '/includes/class-support-helpers.php';
 		include_once dirname( __FILE__ ) . '/includes/class-support-widget.php';
+		include_once dirname( __FILE__ ) . '/includes/class-support-overrides.php';
+		include_once dirname( __FILE__ ) . '/includes/class-support-shortpixel.php';
 		if ( NerdPress_Helpers::is_sucuri_header_set() || NerdPress_Helpers::is_sucuri_firewall_selected() ) {
 			include_once dirname( __FILE__ ) . '/includes/class-support-cloudproxy.php';
 			include_once dirname( __FILE__ ) . '/includes/class-support-clearcache.php';
 		}
+
 		if ( NerdPress_Helpers::is_cloudflare_firewall_selected() ) {
 			include_once dirname( __FILE__ ) . '/includes/class-support-cloudflare.php';
 		}
-		include_once dirname( __FILE__ ) . '/includes/class-support-updates.php';
-		include_once dirname( __FILE__ ) . '/includes/class-support-shortpixel.php';
+		
 	}
 }
 

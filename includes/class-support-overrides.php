f7c5544cdea3ab1fa5f68a8908910d0ec9c99ba8
Andrew Wilder <andrew@nerdpress.net>
2021-08-31 17:27:38 -0700

    Fix exclude_wp_rocket_delay_js
    Add self:: to the option lookup, add $this to the filter, and change the function to public.

--- includes/class-support-overrides.php
+++ includes/class-support-overrides.php
@@ -22,7 +22,7 @@ class NerdPress_Support_Overrides {
 		add_action( 'init', array( $this, 'is_auto_update_set' ) );
 		add_action( 'init', array( $this, 'check_default_options' ) );
 		add_filter( 'wp_mail', array( $this, 'nerdpress_override_alert_email' ) );
-		if ( ! is_admin() && ! isset( $nerdpress_options['exclude_wp_rocket_delay_js'] ) ) {
+		if ( ! is_admin() && ! isset( self::$nerdpress_options['exclude_wp_rocket_delay_js'] ) ) {
 			add_filter( 'rocket_delay_js_exclusions', 'nerdpress_override_rocket_delay_js_exclusions' );
 		}
 
@@ -83,7 +83,7 @@ class NerdPress_Support_Overrides {
 		return $atts;
 	}
 	
-	private function nerdpress_override_rocket_delay_js_exclusions( $excluded_strings = array() ) {
+	public function nerdpress_override_rocket_delay_js_exclusions( $excluded_strings = array() ) {
 		// MUST ESCAPE PERIODS AND PARENTHESES!
 		$excluded_strings[] = 'google-analytics';
 		$excluded_strings[] = "/gtag/";

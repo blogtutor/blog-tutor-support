d69fdc31ecf1ed0f5b59608a696a31a4823eaab0
Andrew Wilder <andrew@nerdpress.net>
2021-11-02 17:15:49 -0700

    Add "ads.mins.js" to exclusion list
    Closes #173

--- includes/class-support-overrides.php
+++ includes/class-support-overrides.php
@@ -95,6 +95,7 @@ class NerdPress_Support_Overrides {
 		$excluded_strings[] = 'gtagTracker'; // Monster Insights
 		$excluded_strings[] = "mediavine";
 		$excluded_strings[] = "adthrive";
+		$excluded_strings[] = "ads.mins.js";
 		$excluded_strings[] = "nutrifox";
 		$excluded_strings[] = "flodesk";
 		$excluded_strings[] = "cp-popup\.js"; // ConvertPro

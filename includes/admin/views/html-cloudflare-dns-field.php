a2816eb2f96d66d7d9702f8ae8794f58aa167c03
Sergio Scabuzzo <sergio@ecotechie.io>
2022-05-24 10:12:15 -0700

    Add $maybe_disable variable

--- includes/admin/views/html-cloudflare-dns-field.php
+++ includes/admin/views/html-cloudflare-dns-field.php
@@ -2,9 +2,14 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-        
+
+$maybe_disable = '';
+
+if ( ! NerdPress_Helpers::is_cloudflare_firewall_selected() ) {
+  $maybe_disable = 'style="opacity:0.5; pointer-events:none;" ';
+}
 ?>
 
-<input type="radio" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="dns1" <?php if( $zone === 'dns1' ) echo 'checked'; ?>>dns1.nerdpress.net<br />
-<input type="radio" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="dns2" <?php if( $zone === 'dns2' ) echo 'checked'; ?>>dns2.nerdpress.net<br />
-<input type="radio" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="dns3" <?php if( $zone === 'dns3' ) echo 'checked'; ?>>dns3.nerdpress.net<br />
+<input type="radio" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="dns1" <?php echo $maybe_disable; if( $zone === 'dns1' ) echo 'checked'; ?>>dns1.nerdpress.net<br />
+<input type="radio" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="dns2" <?php echo $maybe_disable; if( $zone === 'dns2' ) echo 'checked'; ?>>dns2.nerdpress.net<br />
+<input type="radio" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="dns3" <?php echo $maybe_disable; if( $zone === 'dns3' ) echo 'checked'; ?>>dns3.nerdpress.net<br />
--- includes/admin/views/html-cloudflare-token-field.php
+++ includes/admin/views/html-cloudflare-token-field.php
@@ -10,6 +10,12 @@ if ( isset( $options['cloudflare_token'] ) ) {
 } else {
 	$placeholder = '';
 }
+
+$maybe_disable = '';
+
+if ( ! NerdPress_Helpers::is_cloudflare_firewall_selected() ) {
+  $maybe_disable = 'style="opacity:0.5; pointer-events:none;" ';
+}
 ?>
 
-<input type="text" id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="<?php echo esc_attr( $placeholder ); ?>" placeholder="<?php echo esc_attr( $placeholder ); ?>">
+<input type="text" id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="<?php echo esc_attr( $placeholder ); ?>" <?php echo $maybe_disable; ?> placeholder="<?php echo esc_attr( $placeholder ); ?>">

c37eec483e848acf15f488bea037c6690d856e48
Wyatt Kirby <wyatt@apsis.io>
2022-08-17 12:27:46 -0400

    Format snpashot support file

--- includes/class-support-snapshot.php
+++ includes/class-support-snapshot.php
@@ -1,66 +1,72 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	die();
 }
 
-	/**
-	 * NerdPress_Support_Snapshot
-	 *
-	 * @package  NerdPress
-	 * @category Core
-	 * @author Apsis Labs
-	 */
+/**
+ * NerdPress_Support_Snapshot
+ *
+ * @package  NerdPress
+ * @category Core
+ * @author Apsis Labs
+ */
 
-class NerdPress_Support_Snapshot {
-	public static function init() {
+class NerdPress_Support_Snapshot
+{
+	public static function init()
+	{
 		$class = __CLASS__;
 		new $class;
 	}
 
-	public function __construct() {
-		add_action( 'wp_loaded', array( $this, 'ping_relay' ) );
-		add_action( 'wp_loaded', array( $this, 'schedule_snapshot_cron' ) );
+	public function __construct()
+	{
+		add_action('wp_loaded', array($this, 'ping_relay'));
+		add_action('wp_loaded', array($this, 'schedule_snapshot_cron'));
 
-		add_action( 'np_scheduled_snapshot', array( $this, 'take_snapshot'));
+		add_action('np_scheduled_snapshot', array($this, 'take_snapshot'));
 	}
 
 	/**
 	 * Ping the relay server if the PING is set in the GET request.
 	 */
-	public static function ping_relay() {
+	public static function ping_relay()
+	{
 		// If the request is a one-time call from the dashboard.
-		if ( isset( $_GET['np_snapshot'] ) && NerdPress_Helpers::is_relay_server_configured() ) {
+		if (isset($_GET['np_snapshot']) && NerdPress_Helpers::is_relay_server_configured()) {
 			self::take_snapshot();
-			wp_safe_redirect( $_SERVER['HTTP_REFERER'] );
+			wp_safe_redirect($_SERVER['HTTP_REFERER']);
 			die;
 		}
 	}
 
-	public function schedule_snapshot_cron() {
+	public function schedule_snapshot_cron()
+	{
 
-		if ( ! wp_next_scheduled( 'np_scheduled_snapshot' ) ) {
-			wp_schedule_event( time(), 'twicedaily', 'np_scheduled_snapshot' );
+		if (!wp_next_scheduled('np_scheduled_snapshot')) {
+			wp_schedule_event(time(), 'twicedaily', 'np_scheduled_snapshot');
 		}
 
 		// if ( isset( get_option( 'blog_tutor_support_settings' )['schedule_snapshot'] ) ) {
 		// } else {
-		// 	if ( wp_next_scheduled( 'np_scheduled_snapshot' ) ) {
-		// 		wp_clear_scheduled_hook( 'np_scheduled_snapshot' );
-		// 	}
+		//  if ( wp_next_scheduled( 'np_scheduled_snapshot' ) ) {
+		//      wp_clear_scheduled_hook( 'np_scheduled_snapshot' );
+		//  }
 		// }
 	}
 
 	public function take_snapshot()
 	{
 		$dump         = self::assemble_snapshot();
-		$api_response = self::send_request_to_relay( $dump );
+		$api_response = self::send_request_to_relay($dump);
 
 		return $api_response;
 	}
 
-	public static function send_request_to_relay( $dump ) {
+	public static function send_request_to_relay($dump)
+	{
 
-		if ( defined( 'SSLVERIFY_DEV' ) && SSLVERIFY_DEV === false ) {
+		if (defined('SSLVERIFY_DEV') && SSLVERIFY_DEV === false) {
 			$status = false;
 		} else {
 			$status = true;
@@ -75,14 +81,14 @@ class NerdPress_Support_Snapshot {
 				'Authorization' => "Bearer $api_token",
 				"Content-Type" => 'application/json',
 			),
-			'body'      => wp_json_encode( $dump ),
+			'body'      => wp_json_encode($dump),
 			// Bypass SSL verification when using self signed cert. Like when in a local dev environment.
 			'sslverify' => $status,
 		);
 
 
 		// Make request to the relay server.
-		$api_response = wp_remote_post( $relay_url, $args );
+		$api_response = wp_remote_post($relay_url, $args);
 
 		return $api_response;
 	}
@@ -91,35 +97,37 @@ class NerdPress_Support_Snapshot {
 	public static function assemble_snapshot()
 	{
 		// The HTML must be escaped to prevent JSON errors on the relay server.
-		function filter_htmlspecialchars( &$value ) {
-			$value = htmlspecialchars( $value, ENT_QUOTES, 'UTF-8' );
+		function filter_htmlspecialchars(&$value)
+		{
+			$value = htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 		}
 
 		// Check if get_plugins() function exists. This is required on the front end of the
 		// site, since it is in a file that is normally only loaded in the admin.
-		if ( ! function_exists( 'get_plugins' ) ) {
+		if (!function_exists('get_plugins')) {
 			require_once ABSPATH . 'wp-admin/includes/plugin.php';
 		}
 
 		$current_plugins = get_plugins();
 		$mu_plugins = get_mu_plugins();
 		$current_theme   = wp_get_theme();
-		$active_plugins_option = get_option( 'active_plugins' );
+		$active_plugins_option = get_option('active_plugins');
 		$active_plugins = static::filter_active_plugins(get_plugins(), $active_plugins_option);
 		$inactive_plugins = static::filter_inactive_plugins(get_plugins(), $active_plugins_option);
 
-		array_walk_recursive( $current_plugins, 'filter_htmlspecialchars' );
-		array_walk_recursive( $mu_plugins, 'filter_htmlspecialchars' );
-		array_walk_recursive( $active_plugins, 'filter_htmlspecialchars' );
-		array_walk_recursive( $inactive_plugins, 'filter_htmlspecialchars' );
+		array_walk_recursive($current_plugins, 'filter_htmlspecialchars');
+		array_walk_recursive($mu_plugins, 'filter_htmlspecialchars');
+		array_walk_recursive($active_plugins, 'filter_htmlspecialchars');
+		array_walk_recursive($inactive_plugins, 'filter_htmlspecialchars');
 
 		require ABSPATH . WPINC . '/version.php';
 
-		$user                             = wp_parse_url( get_bloginfo( 'wpurl' ) )['host'];
-		$options                          = get_option( 'blog_tutor_support_settings', array() );
+		$user                             = wp_parse_url(get_bloginfo('wpurl'))['host'];
+		$options                          = get_option('blog_tutor_support_settings', array());
+		$disk_space                       = NerdPress_Helpers::get_disk_info()['disk_free'];
 
 		$dump                             = array();
-		$dump['free_disk_space']          = NerdPress_Helpers::format_size( NerdPress_Helpers::get_disk_info()['disk_free'] );
+		$dump['free_disk_space']          = $disk_space == "Unavailable" ? null : $disk_space;
 		$dump['firewall_setting']         = $options['firewall_choice'];
 		$dump['domain']                   = $user;
 		$dump['all_plugins']              = $current_plugins;
@@ -128,22 +136,22 @@ class NerdPress_Support_Snapshot {
 		$dump['inactive_plugins']         = $inactive_plugins;
 		$dump['active_theme']             = $current_theme['Name'];
 		$dump['active_theme_version']     = $current_theme['Version'];
-		$dump['plugin_update_data']       = get_option( '_site_transient_update_plugins' )->response;
+		$dump['plugin_update_data']       = get_option('_site_transient_update_plugins')->response;
 		$dump['wordpress_version']        = $wp_version;
 		$dump['inactive_themes_data']     = wp_get_themes();
 
 		// Removing the active theme from the theme data.
 		$i = -1;
-		foreach ( $dump['inactive_themes_data'] as $key => $value ) {
+		foreach ($dump['inactive_themes_data'] as $key => $value) {
 			$i++;
-			if ( $value['Name'] === $current_theme['Name'] ) {
-				unset( $dump['inactive_themes_data'][ $key ] );
+			if ($value['Name'] === $current_theme['Name']) {
+				unset($dump['inactive_themes_data'][$key]);
 			}
 		}
 
 		// The notes field is NULL on first install, so we check if it's present.
-		if ( isset( get_option( 'blog_tutor_support_settings' )['admin_notice'] ) ) {
-			$dump['notes'] = get_option( 'blog_tutor_support_settings' )['admin_notice'];
+		if (isset(get_option('blog_tutor_support_settings')['admin_notice'])) {
+			$dump['notes'] = get_option('blog_tutor_support_settings')['admin_notice'];
 		}
 
 		return $dump;
@@ -160,5 +168,4 @@ class NerdPress_Support_Snapshot {
 	}
 }
 
-add_action( 'init', array( 'NerdPress_Support_Snapshot', 'init' ) );
-
+add_action('init', array('NerdPress_Support_Snapshot', 'init'));

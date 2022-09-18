528478825eb40dfbfd174cb31fe10c400da2f93c
Sergio Scabuzzo <sergio@ecotechie.io>
2022-09-18 11:10:06 -0700

    Clean up logic. Closes #204. Closes #203.

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -43,7 +43,14 @@ function bt_custom_toolbar_links( $wp_admin_bar ) {
 						),
 					);
 					$wp_admin_bar->add_node( $args );
-					if ( ! is_admin() && ! NerdPress_Helpers::cache_clear_bypass_on_string( array( $_SERVER['REQUEST_URI'] ) ) !== false ) {
+
+					if (
+						! is_admin() &&
+						(
+							! NerdPress_Helpers::cache_clear_bypass_on_string( array( $_SERVER['REQUEST_URI'] ) ) !== false ||
+							strpos( $_SERVER['REQUEST_URI'], 'lang=' ) !== false
+						)
+				   	) {
 						$args = array(
 							'id'     => 'nerdpress-purge-url',
 							'title'  => 'Purge this URL from Cloudflare',
--- includes/class-support-cloudflare.php
+++ includes/class-support-cloudflare.php
@@ -380,7 +380,7 @@ class NerdPress_Cloudflare_Client {
 			$prefixes_no_protocol   = preg_replace( '#https?://#', '', $prefixes );
 
 			self::$cache_clear_type = implode( ',', $prefixes_no_protocol );
-			$body                   = '{ "prefixes": [' . implode( ',', $prefixes_no_protocol ) . '] }';
+			$body                   = '{ "prefixes": ["' . implode( ',', $prefixes_no_protocol ) . '"] }';
 		}
 
 		$url  = self::assemble_url() . 'purge_cache';
@@ -510,7 +510,7 @@ class NerdPress_Cloudflare_Client {
 
 		$url_clear               = esc_url( $_POST['url'] );
 		self::$cache_trigger_url = $url_clear;
-		echo self::purge_cloudflare_cache( array( '"' . $url_clear . '"' ) );
+		echo self::purge_cloudflare_cache( array( $url_clear ) );
 		die();
 	}
 
@@ -549,7 +549,7 @@ class NerdPress_Cloudflare_Client {
 		self::$status_before     = $old_status;
 		self::$status_after      = $new_status;
 
-		self::purge_cloudflare_cache( array( '"' . get_permalink( $post_id ) . '"' ) );
+		self::purge_cloudflare_cache( array( get_permalink( $post_id ) ) );
 	}
 
 	/**
@@ -583,7 +583,7 @@ class NerdPress_Cloudflare_Client {
 		self::$status_before     = 'new comment';
 		self::$status_after      = 'approved';
 
-		self::purge_cloudflare_cache( array( '"' . get_permalink( $post_id ) . '"' ) );
+		self::purge_cloudflare_cache( array( get_permalink( $post_id ) ) );
 	}
 
 	/**
@@ -612,7 +612,7 @@ class NerdPress_Cloudflare_Client {
 		self::$status_before     = 'approved';
 		self::$status_after      = ( $data['comment_approved'] ? $data['comment_approved'] : 'pending' );
 
-		self::purge_cloudflare_cache( array( '"' . get_permalink( $post_id ) . '"' ) );
+		self::purge_cloudflare_cache( array( get_permalink( $post_id ) ) );
 	}
 
 	/**

8c0f92cad805030e30f05d1592ab34550dfdd3be
Jon Christopher <jon@jonchristopher.us>
2023-03-20 20:27:34 -0400

    Adds additional hardening to variable output

--- includes/admin/views/html-nerdpress-relay-server-url-field.php
+++ includes/admin/views/html-nerdpress-relay-server-url-field.php
@@ -14,4 +14,4 @@ if ( isset( $options['np_relay_server_url'] ) ) {
 
 <input type="text" id="<?php echo esc_attr( $id ); ?>" name="<?php echo esc_attr( $menu ) ?>[<?php echo esc_attr( $id ); ?>]" value="<?php echo esc_attr( $placeholder ); ?>" placeholder="<?php echo esc_attr( $placeholder ); ?>">
 
-<p class="description">If blank, URL will be set to <strong><?php echo NerdPress_Helpers::relay_server_default_url(); ?></strong></p>
+<p class="description">If blank, URL will be set to <strong><?php echo esc_html( NerdPress_Helpers::relay_server_default_url() ); ?></strong></p>
--- includes/admin/views/html-serverinfo-field.php
+++ includes/admin/views/html-serverinfo-field.php
@@ -32,7 +32,7 @@ if ( function_exists( 'sys_getloadavg' ) ) {
 	if ( $loads ) {
 		?>
 		<article>
-			<h2 style="margin-top: 0;">Load Averages: &nbsp; <?php echo $loads[0] . ' &nbsp; ' . $loads[1] . ' &nbsp; ' . $loads[2]; ?></h2>
+			<h2 style="margin-top: 0;">Load Averages: &nbsp; <?php echo esc_html( $loads[0] ) . ' &nbsp; ' . esc_html( $loads[1] ) . ' &nbsp; ' . esc_html( $loads[2] ); ?></h2>
 		</article>
 		<?php
 	}
@@ -42,13 +42,13 @@ if ( function_exists( 'sys_getloadavg' ) ) {
 
 <article>
 	<h2>Disk Space:</h2>
-	<p>Total: <?php echo $disk_total; ?></p>
+	<p>Total: <?php echo esc_html( $disk_total ); ?></p>
 	<div class='progress'>
-		<div class='prgtext <?php if ( NerdPress_Helpers::get_disk_info()['disk_percentage'] > 90 ) echo 'prgtext-danger'; ?>'><?php echo NerdPress_Helpers::get_disk_info()['disk_percentage']; ?>% Used</div>
-		<div class='prgbar-disk <?php if ( NerdPress_Helpers::get_disk_info()['disk_percentage'] > 90 ) echo 'prgbar-danger'; ?>' style="width: <?php echo NerdPress_Helpers::get_disk_info()['disk_percentage']; ?>%;"></div>
+		<div class='prgtext <?php if ( NerdPress_Helpers::get_disk_info()['disk_percentage'] > 90 ) echo 'prgtext-danger'; ?>'><?php echo esc_html( NerdPress_Helpers::get_disk_info()['disk_percentage'] ); ?>% Used</div>
+		<div class='prgbar-disk <?php if ( NerdPress_Helpers::get_disk_info()['disk_percentage'] > 90 ) echo 'prgbar-danger'; ?>' style="width: <?php echo esc_html( NerdPress_Helpers::get_disk_info()['disk_percentage'] ); ?>%;"></div>
 		<div class='prginfo'>
-			<span style='float: left;'><?php echo $disk_used . ' used'; ?></span>
-			<span style='float: right;'><?php echo $disk_free . ' free'; ?></span>
+			<span style='float: left;'><?php echo esc_html( $disk_used ) . ' used'; ?></span>
+			<span style='float: right;'><?php echo esc_html( $disk_free ) . ' free'; ?></span>
 			<span style='clear: both;'></span>
 		</div>
 	</div>
@@ -75,13 +75,13 @@ if ( array_key_exists( 'MemTotal', $mem_info ) ) {
 ?>
 	<article>
 		<h2>Memory Usage:</h2>
-		<p>Total: <?php echo $mem_total; ?></p>
+		<p>Total: <?php echo esc_html( $mem_total ); ?></p>
 		<div class='progress'>
-			<div class='prgtext <?php echo $prgtext_danger; ?>'><?php echo $mem_percentage; ?>% Used</div>
-			<div class='prgbar-mem <?php echo $prgbar_danger; ?>' style='width: <?php echo $mem_percentage; ?>%;'></div>
+			<div class='prgtext <?php echo esc_html( $prgtext_danger ); ?>'><?php echo esc_html( $mem_percentage ); ?>% Used</div>
+			<div class='prgbar-mem <?php echo esc_html( $prgbar_danger ); ?>' style='width: <?php echo esc_html( $mem_percentage ); ?>%;'></div>
 			<div class='prginfo'>
-				<span style='float: left;'><?php echo "$mem_used used"; ?></span>
-				<span style='float: right;'><?php echo "$mem_available free"; ?></span>
+				<span style='float: left;'><?php echo esc_html( $mem_used ); ?> used</span>
+				<span style='float: right;'><?php echo esc_html( $mem_available ); ?> free</span>
 				<span style='clear: both;'></span>
 			</div>
 		</div>
@@ -92,17 +92,17 @@ if ( array_key_exists( 'MemTotal', $mem_info ) ) {
 
 <article>
 	<h2>Server Information:</h2>
-	<p><strong>PHP VERSION:</strong> <?php echo phpversion(); ?></p>
-	<p><strong>SERVER_SOFTWARE:</strong> <?php if ( ! empty( $_SERVER['SERVER_SOFTWARE'] ) ) { echo $_SERVER['SERVER_SOFTWARE']; } ?></p>
-	<p><strong>SERVER_PROTOCOL:</strong> <?php if ( ! empty( $_SERVER['SERVER_PROTOCOL'] ) ) { echo $_SERVER['SERVER_PROTOCOL']; } ?></p>
-	<p><strong>DOCUMENT_ROOT:</strong> <?php if ( ! empty( $_SERVER['DOCUMENT_ROOT'] ) ) { echo $_SERVER['DOCUMENT_ROOT']; } ?></p>
-	<p><strong>REMOTE_ADDR:</strong> <?php if ( ! empty( $_SERVER['REMOTE_ADDR'] ) ) { echo $_SERVER['REMOTE_ADDR']; } ?></p>
-	<p><strong>HTTP_UPGRADE_INSECURE_REQUESTS:</strong> <?php if ( ! empty( $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] ) ) { echo $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS']; } ?></p>
-	<P><strong>HTTP_CLIENT_IP:</strong> <?php if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) { echo $_SERVER['HTTP_CLIENT_IP']; } else { echo '(not set)'; } ?></p>
-	<p><strong>HTTP_X_SUCURI_CLIENTIP:</strong> <?php if ( ! empty( $_SERVER['HTTP_X_SUCURI_CLIENTIP'] ) ) { echo $_SERVER['HTTP_X_SUCURI_CLIENTIP']; } ?></p>
-	<p><strong>HTTP_X_FORWARDED_FOR:</strong> <?php if ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) { echo $_SERVER['HTTP_X_FORWARDED_FOR']; } ?></p>
-	<p><strong>HTTP_X_REAL_IP:</strong> <?php if ( ! empty( $_SERVER['HTTP_X_REAL_IP'] ) ) { echo $_SERVER['HTTP_X_REAL_IP']; } ?></p>
-	<p><strong>SERVER_ADDR:</strong> <?php if ( ! empty( $_SERVER['SERVER_ADDR'] ) ) { echo $_SERVER['SERVER_ADDR']; } ?></p>
+	<p><strong>PHP VERSION:</strong> <?php echo esc_html( phpversion() ); ?></p>
+	<p><strong>SERVER_SOFTWARE:</strong> <?php if ( ! empty( $_SERVER['SERVER_SOFTWARE'] ) ) { echo esc_html( $_SERVER['SERVER_SOFTWARE'] ); } ?></p>
+	<p><strong>SERVER_PROTOCOL:</strong> <?php if ( ! empty( $_SERVER['SERVER_PROTOCOL'] ) ) { echo esc_html( $_SERVER['SERVER_PROTOCOL'] ); } ?></p>
+	<p><strong>DOCUMENT_ROOT:</strong> <?php if ( ! empty( $_SERVER['DOCUMENT_ROOT'] ) ) { echo esc_html( $_SERVER['DOCUMENT_ROOT'] ); } ?></p>
+	<p><strong>REMOTE_ADDR:</strong> <?php if ( ! empty( $_SERVER['REMOTE_ADDR'] ) ) { echo esc_html( $_SERVER['REMOTE_ADDR'] ); } ?></p>
+	<p><strong>HTTP_UPGRADE_INSECURE_REQUESTS:</strong> <?php if ( ! empty( $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] ) ) { echo esc_html( $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'] ); } ?></p>
+	<P><strong>HTTP_CLIENT_IP:</strong> <?php if ( ! empty( $_SERVER['HTTP_CLIENT_IP'] ) ) { echo esc_html( $_SERVER['HTTP_CLIENT_IP'] ); } else { echo '(not set)'; } ?></p>
+	<p><strong>HTTP_X_SUCURI_CLIENTIP:</strong> <?php if ( ! empty( $_SERVER['HTTP_X_SUCURI_CLIENTIP'] ) ) { echo esc_html( $_SERVER['HTTP_X_SUCURI_CLIENTIP'] ); } ?></p>
+	<p><strong>HTTP_X_FORWARDED_FOR:</strong> <?php if ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) { echo esc_html( $_SERVER['HTTP_X_FORWARDED_FOR'] ); } ?></p>
+	<p><strong>HTTP_X_REAL_IP:</strong> <?php if ( ! empty( $_SERVER['HTTP_X_REAL_IP'] ) ) { echo esc_html( $_SERVER['HTTP_X_REAL_IP'] ); } ?></p>
+	<p><strong>SERVER_ADDR:</strong> <?php if ( ! empty( $_SERVER['SERVER_ADDR'] ) ) { echo esc_html( $_SERVER['SERVER_ADDR'] ); } ?></p>
 </article>
 
 <br>

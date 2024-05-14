fe4c578bdacc2f2be771a1b02ebec6094b067648
Sergio Scabuzzo <sergio@ecotechie.io>
2024-05-14 15:10:12 -0700

    Fix undefined index warning. Closes #317

--- includes/class-support-helpers.php
+++ includes/class-support-helpers.php
@@ -411,7 +411,7 @@ class NerdPress_Helpers {
 
 	public static function relay_server_url() {
 		$options = get_option( 'blog_tutor_support_settings', array() );
-		$url     = $options['np_relay_server_url'] ? $options['np_relay_server_url'] : self::relay_server_default_url();
+		$url     = ! empty( $options['np_relay_server_url'] ) ? $options['np_relay_server_url'] : self::relay_server_default_url();
 		return trailingslashit( $url );
 	}
 

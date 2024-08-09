a8a4732b07b8a0778c87510a8be5e4c5c20d2591
Brian Routzong <60560502+1337rout@users.noreply.github.com>
2024-08-09 12:26:12 -0500

    Safely add the query variable - Thanks @ecotechie and @jchristopher
    Co-authored-by: Jon Christopher <jon@jonchristopher.us>

--- includes/class-support-widget.php
+++ includes/class-support-widget.php
@@ -119,7 +119,7 @@ class NerdPress_Widget {
 			$status = true;
 		}
 
-		$relay_url = NerdPress_Helpers::relay_server_url() . 'wp-json/nerdpress/v1/client-display-traffic?timezone='. esc_url( wp_timezone_string() );
+		$relay_url = esc_url( add_query_arg( array( 'timezone' => wp_timezone_string() ), NerdPress_Helpers::relay_server_url() . 'wp-json/nerdpress/v1/client-display-traffic' ) );
 		$api_token = NerdPress_Helpers::relay_server_api_token();
 
 		$args = array(
